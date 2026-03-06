4e310b9121b5ec3407fe895475eb40beb1de07d0
Snorri <din.epost@example.com>
2026-03-06 17:02:46 +0100

    finished 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,8 +10,16 @@ const Content = (props) => {
   console.log(Content)
   return (
     <div>
-      <p>{props.part} {props.exercises}</p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
@@ -38,9 +46,9 @@ const App = () => {
     <div>
       <Header course={course} />
 
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part1={part1} part2={part2} part3={part3} 
+              exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}
+      />
 
       <Total totalExercises={exercises1 + exercises2 + exercises3} />
     </div>
