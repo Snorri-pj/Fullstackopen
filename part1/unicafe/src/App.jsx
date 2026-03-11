261e7bed063ff4e4b26e9723d9067bd3c3184589
Snorri <din.epost@example.com>
2026-03-11 20:21:20 +0100

    finished 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const Button = (props) => 
+  <button onClick={props.onClick}>{props.text}</button>
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = (newValue) => setGood(newValue)
+  const setToNeutral = (newValue) => setNeutral(newValue)
+  const setToBad = (newValue) => setBad(newValue)
+  
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => setToGood(good + 1)} text='good' />
+      <Button onClick={() => setToNeutral(neutral + 1)} text='neutral' />
+      <Button onClick={() => setToBad(bad + 1)} text='bad' />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
