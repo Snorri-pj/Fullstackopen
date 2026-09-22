1c75a1e5117fb554a9d06a01ba2e51b258873056
Snorri <din.epost@example.com>
2026-09-22 19:17:58 +0200

    finished 2.13 already

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import AddNew from './components/AddNew'
 import RenderPerson from './components/RenderPerson'
